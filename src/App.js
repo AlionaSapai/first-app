423c6b6d8225b9d0a5208d39a5d52d8f790cd42d
Алена <alioshe4ka@gmail.com>
2022-03-30 23:14:56 +0300

    v1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import logo from './logo.svg';
+import {Button} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./Components/Header";
+import './App.css';
+
+
+function App() {
+  return (
+      <div>
+        <Header/>
+      </div>
+  );
+}
+
+export default App;
+
+
