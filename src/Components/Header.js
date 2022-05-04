658287c942b5546ca4f3aee6938bc10bf4ed41d1
Алена <alioshe4ka@gmail.com>
2022-05-04 23:59:26 +0300

    v1.2

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,12 @@
-import React, { Component } from "react";
+import React, {Component, useState} from "react";
 import {
     Navbar,
     Nav,
     FormControl,
     Container,
     Form,
-    Button
+    Button,
+    Modal
 } from "react-bootstrap";
 import logo from "./logo192.png";
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
@@ -15,8 +16,14 @@ import Contacts from "../Pages/Contacts";
 import About from "../Pages/About";
 import Blog from "../Pages/Blog";
 
-export default class Header extends Component{
-    render () {
+
+
+export default function Header(){
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
         return (
             <>
             <Navbar fixed= "top" collapseOnSelect expand="md" bg="dark" variant="dark" >
@@ -38,18 +45,52 @@ export default class Header extends Component{
                             <Nav.Link href="/contacts"> Contacts </Nav.Link>
                             <Nav.Link href="/blog"> Blog </Nav.Link>
                         </Nav>
-                        <Form className="d-flex">
+                        <Form className="d-inline-flex">
                             <FormControl
                                 type="text"
                                 placeholder="Search"
-                                className="me-2"
+                                className="me-5"
                                 />
                             <Button variant="outline-info"> Search </Button>
                         </Form>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-                <Router >
+
+                <Button className="me-2" onClick={handleShow}>Login</Button>
+
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Log in</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        <Form>
+                            <Form.Group controlId="fromBasicEmail">
+                                <Form.Label>Email Address</Form.Label>
+                                <Form.Control type="email" placeholder="Enter Email"/>
+                                <Form.Text className="text-muted">
+                                    We`ll never share your email with anyone else.
+                                </Form.Text>
+                            </Form.Group>
+
+                            <Form.Group controlId="fromBasicPassword">
+                                <Form.Label>Password</Form.Label>
+                                <Form.Control type="password" placeholder="Enter password">
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId="fromBasicCheckbox">
+                                <Form.Check type="checkbox" label="Remember me"/>
+                            </Form.Group>
+
+                            <Button variant="primary" type="submit">
+                                Submit
+                            </Button>
+                        </Form>
+                    </Modal.Body>
+                </Modal>
+
+                <Router>
                     <Routes>
                         <Route path="/" element={<Home />}/>
                         <Route path="/about" element={<About/>}/>
@@ -60,4 +101,10 @@ export default class Header extends Component{
             </>
         )
     }
-}
+/*export default function Header(){
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+}*/
+
